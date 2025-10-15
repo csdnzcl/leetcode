1cbac2e41b518ebba1068cab900205cebd676b8f
csdnzcl <csdnzhou@outlook.com>
2025-10-15 18:49:27 +0800

    2025-10-15

---
+++ src/162.寻找峰值.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=java
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = -1;
+        int right = nums.length - 1; // 开区间 (-1, n-1)
+        while (left + 1 < right) { // 开区间不为空
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return right;
+    }
+}
+
+// @lc code=end
+
